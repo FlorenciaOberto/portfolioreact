11b210cbbd9a40ba5dc4d099adf872a75432619d
Florencia Oberto <obertoflorencia@gmail.com>
2024-03-29 21:17:38 +0100

    first commit

---
+++ src/portfolioapp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const portfolioapp = () => {
+  return (
+    <div><h1>Este es mi portafolio</h1>
+    <h2>este es mi primer componente</h2>
+    <p>hola mundo</p>
+    </div>
+  )
+}
